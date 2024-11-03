0ed61e7c0dff980132c2c6047984af65d49f227d
EiEiKyaw <11eikyw@gmail.com>
2024-11-03 11:56:26 +0630

    feat: AppDbContext

---
+++ EEKDotNetCore.ConsoleApp/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EEKDotNetCore.ConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEKDotNetCore.ConsoleApp
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Data Source=.;Initial Catalog=EEKDotNetCore;User ID=sa;Password=sa@123;TrustServerCertificate=True;";
+                optionsBuilder.UseSqlServer(connectionString);
+            };
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+
+}
