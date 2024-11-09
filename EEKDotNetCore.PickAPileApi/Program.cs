cdbe74a2e3fd39622cde529c77f583ec52e3ae6e
EiEiKyaw <11eikyw@gmail.com>
2024-11-09 19:21:23 +0630

    feat: pick a pile minimal api Read

--- EEKDotNetCore.PickAPileApi/Program.cs
+++ EEKDotNetCore.PickAPileApi/Program.cs
@@ -18,26 +18,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast")
-//.WithOpenApi();
-
 app.MapGet("/pickapile", () =>
 {
     string folderPath = "Data/PickAPile.json";
@@ -63,11 +43,6 @@ app.MapGet("/pickapile/answers/{id}", (int id) =>
 .WithOpenApi();
 
 app.Run();
-
-//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
 
 
 public class PickAPileresponseModel
