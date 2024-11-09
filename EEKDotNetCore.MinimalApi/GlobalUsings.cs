92d243b81cc2faa56f3278acb60eb549c6d9cf11
EiEiKyaw <11eikyw@gmail.com>
2024-11-09 13:59:15 +0630

    feat: minimal api CRUD

---
+++ EEKDotNetCore.MinimalApi/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using EEKDotNetCore.Database.Models;
+global using Microsoft.EntityFrameworkCore;
+global using System.Runtime.CompilerServices;
+global using EEKDotNetCore.MinimalApi.Endpoints.Blog;
+global using System.Reflection.Metadata;
+
