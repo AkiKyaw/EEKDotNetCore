4ad4ac32ecf9457aa924fb1f1dbc38a8a22f21c6
EiEiKyaw <11eikyw@gmail.com>
2024-11-10 22:16:46 +0630

    feat: Minimal API Update, Delete

--- EEKDotNetCore.PickAPileApi/Program.cs
+++ EEKDotNetCore.PickAPileApi/Program.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System.Reflection.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,12 +24,12 @@ app.MapGet("/pickapile", () =>
     string folderPath = "Data/PickAPile.json";
     var jsonStr = File.ReadAllText(folderPath);
     var result = JsonConvert.DeserializeObject<PickAPileresponseModel>(jsonStr)!;
-    return Results.Ok(result.Questions);
+    return Results.Ok(result);
 })
-.WithName("GetQuestions")
+.WithName("GetPickAPile")
 .WithOpenApi();
 
-app.MapGet("/pickapile/answers/{id}", (int id) =>
+app.MapGet("/pickapile/{id}", (int id) =>
 {
     string folderPath = "Data/PickAPile.json";
     var jsonStr = File.ReadAllText(folderPath);
@@ -42,13 +43,95 @@ app.MapGet("/pickapile/answers/{id}", (int id) =>
 .WithName("GetAnswer")
 .WithOpenApi();
 
+app.MapPost("/pickapile", (QuestionModel requestModel) =>
+{
+    string folderPath = "Data/PickAPile.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<PickAPileresponseModel>(jsonStr)!;
+
+    requestModel.QuestionId = result.Questions.Count == 0 ? 1 : result.Questions.Max(x => x.QuestionId) + 1;
+    result.Questions.Add(requestModel);
+
+    var jsonStrToWrite = JsonConvert.SerializeObject(result);
+    File.WriteAllText(folderPath, jsonStrToWrite);
+
+    return Results.Ok(requestModel);
+})
+.WithName("CreateQuestion")
+.WithOpenApi();
+
+app.MapPut("/pickapile/{id}", (int id, QuestionModel requestModel) =>
+{
+    string folderPath = "Data/PickAPile.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<PickAPileresponseModel>(jsonStr)!;
+
+    var item = result.Questions.FirstOrDefault(x => x.QuestionId == id);
+    if (item is null) return Results.BadRequest("No data found.");
+
+    item.QuestionName = requestModel.QuestionName;
+    item.QuestionDesp = requestModel.QuestionDesp;
+
+    var jsonStrToWrite = JsonConvert.SerializeObject(result);
+    File.WriteAllText(folderPath, jsonStrToWrite);
+
+    return Results.Ok(item);
+})
+.WithName("UpdateQuestion")
+.WithOpenApi();
+
+app.MapPatch("/pickapile/{id}", (int id, QuestionModel requestModel) =>
+{
+    string folderPath = "Data/PickAPile.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<PickAPileresponseModel>(jsonStr)!;
+
+    var item = result.Questions.FirstOrDefault(x => x.QuestionId == id);
+    if (item is null) return Results.BadRequest("No data found.");
+
+    if (!string.IsNullOrEmpty(requestModel.QuestionName))
+    {
+        item.QuestionName = requestModel.QuestionName;
+    }
+    if (!string.IsNullOrEmpty(requestModel.QuestionDesp))
+    {
+        item.QuestionDesp = requestModel.QuestionDesp;
+    }
+
+    var jsonStrToWrite = JsonConvert.SerializeObject(result);
+    File.WriteAllText(folderPath, jsonStrToWrite);
+
+    return Results.Ok(item);
+})
+.WithName("PatchQuestion")
+.WithOpenApi();
+
+app.MapDelete("/pickapile/{id}", (int id) =>
+{
+    string folderPath = "Data/PickAPile.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<PickAPileresponseModel>(jsonStr)!;
+
+    var item = result.Answers.FirstOrDefault(x => x.AnswerId == id);
+    if (item is null) return Results.BadRequest("No data found.");
+
+    result.Answers.Remove(item);
+
+    var jsonStrToWrite = JsonConvert.SerializeObject(result);
+    File.WriteAllText(folderPath, jsonStrToWrite);
+
+    return Results.Ok("Deleted");
+})
+.WithName("DeleteAnswer")
+.WithOpenApi();
 app.Run();
+
 
 
 public class PickAPileresponseModel
 {
-    public QuestionModel[] Questions { get; set; }
-    public AnswerModel[] Answers { get; set; }
+    public List<QuestionModel> Questions { get; set; }
+    public List<AnswerModel> Answers { get; set; }
 }
 
 public class QuestionModel
